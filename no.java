00660ae4a0dd3f4e212b307226c0cd2c7aecb921
alia720 <alialyasseen@gmail.com>
2020-07-21 15:48:14 -0600

    no

--- no.java
+++ no.java
@@ -5,6 +5,7 @@ public class no {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("no");
+        System.out.println("yes");
+        
     }
 }
